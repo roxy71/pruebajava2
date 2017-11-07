316aeef95f0f1174c1fa2ab59d983840ef6682e4
roxy71 <roxana71janet@gmail.com>
2017-11-06 21:17:57 -0300

    Aca se crea la clave y contraseña

---
+++ src/pruebajava2/Contraseña.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava2;
+import javax.swing.JOptionPane;
+
+/** *Roxana Rojo Bustamante
+ * Fecha 6 de Noviembre 2017
+ * @author LAB03
+ * Clase pruebajava2
+ * Responsabilidad clase principal
+ *
+ * @author LAB03
+ 
+ */
+public class Contraseña 
+{
+  public static void main(String[] args) 
+  {
+      int Contraseña =0;
+      int numero = 3;
+      int index =0;
+     
+          int clave;  
+          clave = 1234;
+          
+            while(index<=3)
+               
+      
+          
+      Contraseña = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Contraseña"));
+     
+      {
+      if (Contraseña ==1234 Contraseña Correcta");
+        
+      {
+        JOptionPane.showMessageDialog(null, "Contraseña Correcta");
+       
+        
+      }
+      
+      else 
+      
+      {
+           if Contraseña!= 1234 ("Contraseña Incorrecta");
+               
+               
+               
+            JOptionPane.showMessageDialog(null, "Contraseña Incorrecta");
+           
+           
+                    
+  }
+  JOptionPane.showMessageDialog(null, "Cuenta bloqueada,comuniquese con soporte tecnico"); 
+}
+
+  }
+  
+  
+  }
